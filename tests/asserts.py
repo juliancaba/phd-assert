1f3c3f333bc219771d33250bdcc7134b7514db2b
julian <julian@noyce.uclm.es>
2017-06-06 12:10:34 +0200

    Fixed bugs

--- tests/asserts.py
+++ tests/asserts.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8; mode: python -*-
 
 import sys
-from hwt_proxy import FPGA_hwtClient
+from hwt_proxy import sendMessage
 from arm_casting_ieee754 import *
 
 
@@ -15,48 +15,18 @@ _float_lt_failurecount = 0
 _float_lt_failurecount_vld = 0
 
 
-def ASSERT_FLOAT_EQ_CALLCOUNT():      
-    din = []
-    din.extend(int_to_byte(0x000B1700))
-    din.extend(int_to_byte(0x00000000))
-
-    testCli = FPGA_hwtClient()
-    testCli.arguments(_hw_addr, din)
-    testCli.main([None])
-    dout = testCli.result()
-    idout = charSeq_to_intSeq(dout)
-
-    head1 = idout[0]
-    head2 = idout[1]
-
-    _callcount = idout[2]
-    del testCli
-    del idout
+def ASSERT_FLOAT_EQ_CALLCOUNT():          
+    head,payload = sendMessage(0x42000000, 0x000B1700, 0x00000000, None)
+    _callcount = int(payload[0],16)
     return _callcount
 
 
   
-def ASSERT_FLOAT_EQ_FAILURECOUNT():      
-    din = []
-    din.extend(int_to_byte(0x000B1800))
-    din.extend(int_to_byte(0x00000000))
-
-    testCli = FPGA_hwtClient()
-    testCli.arguments(_hw_addr, din)
-    testCli.main([None])
-    dout = testCli.result()
-    idout = charSeq_to_intSeq(dout)
-
-    head1 = idout[0]
-    head2 = idout[1]
-
-    _float_eq_failurecount = idout[2]
+def ASSERT_FLOAT_EQ_FAILURECOUNT():          
+    head,payload = sendMessage(_hw_addr, 0x000B1800, 0x00000000, None)
+    _float_eq_failurecount = int(payload[0],16)
     _float_eq_failurecount_vld = 1
-    del testCli
-    del idout
     return _float_eq_failurecount
-
-
 
   
 def PRINT_FAILURES_ASSERT_FLOAT_EQ():
@@ -66,77 +36,31 @@ def PRINT_FAILURES_ASSERT_FLOAT_EQ():
         failures = ASSERT_FLOAT_EQ_FAILURECOUNT();
 
     if failures == 0:
-        print("{0}: No failures\n").format(__name__)
+        print("{0}: No failures".format(PRINT_FAILURES_ASSERT_FLOAT_EQ.__name__))
     else:
-        for it in (0,failures):
-            din = []
-            din.extend(int_to_byte(0x000B1A00))
-            din.extend(int_to_byte(0x00000000))
-
-            testCli = FPGA_hwtClient()
-            testCli.arguments(_hw_addr, din)
-            testCli.main([None])
-            dout = testCli.result()
-            idout = charSeq_to_intSeq(dout)
-
-            head1 = idout[0]
-            head2 = idout[1]
-            callCount = idout[2]
-            print ("\tcallCount {0}\n").format(callCount)
-            time = idout[3]
-            print ("\tTime {0}\n").format(time)
-            expected = ieee754_to_float(idout[4])
-            print ("\tExpected {0}\n").format(expected)
-            actual = ieee754_to_float(idout[5])
-            print ("\tActual {0}\n").format(actual)
-
-            del testCli
-            del idout
-
+        for it in range(0,failures):
+            head,payload = sendMessage(_hw_addr, 0x000B1A00, 0x00000000, None)
+            callCount = payload[0]
+            print ("\tcallCount {0}".format(callCount))
+            time = payload[1]
+            print ("\tTime {0}".format(time))
+            expected = ieee754_to_float(payload[2])
+            print ("\tExpected {0}".format(expected))
+            actual = ieee754_to_float(payload[3])
+            print ("\tActual {0}".format(actual))
 
             
-def ASSERT_FLOAT_LT_CALLCOUNT():      
-    din = []
-    din.extend(int_to_byte(0x000B1700))
-    din.extend(int_to_byte(0x00000000))
-
-    testCli = FPGA_hwtClient()
-    testCli.arguments(_hw_addr, din)
-    testCli.main([None])
-    dout = testCli.result()
-    idout = charSeq_to_intSeq(dout)
-
-    head1 = idout[0]
-    head2 = idout[1]
-
-    _callcount = idout[2]
-    del testCli
-    del idout
+def ASSERT_FLOAT_LT_CALLCOUNT():
+    head,payload = sendMessage(_hw_addr, 0x000B2B00, 0x00000000, None)
+    _callcount = int(payload[0],16)
     return _callcount
 
-
   
-def ASSERT_FLOAT_LT_FAILURECOUNT():      
-    din = []
-    din.extend(int_to_byte(0x000B1800))
-    din.extend(int_to_byte(0x00000000))
-
-    testCli = FPGA_hwtClient()
-    testCli.arguments(_hw_addr, din)
-    testCli.main([None])
-    dout = testCli.result()
-    idout = charSeq_to_intSeq(dout)
-
-    head1 = idout[0]
-    head2 = idout[1]
-
-    _float_lt_failurecount = idout[2]
+def ASSERT_FLOAT_LT_FAILURECOUNT():
+    head,payload = sendMessage(_hw_addr, 0x000B2C00, 0x00000000, None)
+    _float_lt_failurecount = int(payload[0],16)
     _float_lt_failurecount_vld = 1
-    del testCli
-    del idout
     return _float_lt_failurecount
-
-
 
   
 def PRINT_FAILURES_ASSERT_FLOAT_LT():
@@ -146,31 +70,17 @@ def PRINT_FAILURES_ASSERT_FLOAT_LT():
         failures = ASSERT_FLOAT_LT_FAILURECOUNT();
 
     if failures == 0:
-        print("{0}: No failures\n").format(__name__)
+        print("{0}: No failures".format(PRINT_FAILURES_ASSERT_FLOAT_LT.__name__))
     else:
-        for it in (0,failures):
-            din = []
-            din.extend(int_to_byte(0x000B1A00))
-            din.extend(int_to_byte(0x00000000))
-
-            testCli = FPGA_hwtClient()
-            testCli.arguments(_hw_addr, din)
-            testCli.main([None])
-            dout = testCli.result()
-            idout = charSeq_to_intSeq(dout)
-
-            head1 = idout[0]
-            head2 = idout[1]
-            callCount = idout[2]
-            print ("\tcallCount {0}\n").format(callCount)
-            time = idout[3]
-            print ("\tTime {0}\n").format(time)
-            expected = ieee754_to_float(idout[4])
-            print ("\tExpected {0}\n").format(expected)
-            actual = ieee754_to_float(idout[5])
-            print ("\tActual {0}\n").format(actual)
-
-            del testCli
-            del idout
+        for it in range(0,failures):
+            head,payload = sendMessage(_hw_addr, 0x000B2E00, 0x00000000, None)
+            callCount = payload[0]
+            print ("\tcallCount {0}".format(callCount))
+            time = payload[1]
+            print ("\tTime {0}".format(time))
+            expected = ieee754_to_float(payload[2])
+            print ("\tExpected {0}".format(expected))
+            actual = ieee754_to_float(payload[3])
+            print ("\tActual {0}".format(actual))
 
 
--- tests/test/test_vector_norm.py
+++ tests/test/test_vector_norm.py
@@ -13,12 +13,7 @@ from asserts import *
 class TestL2Norm(TimeTestCase):
 
     def test_l2norm(self):
-        ref = [0.0, 0.027164, 0.054328,
-               0.081492, 0.108655, 0.135819,
-               0.162983, 0.190147, 0.217311,
-               0.244475, 0.271639, 0.298802,
-               0.325966, 0.353130, 0.380294,
-               0.407458];
+        ref = [0.0, 0.027163850143551826, 0.05432770028710365, 0.08149155229330063, 0.1086554005742073, 0.13581925630569458, 0.16298310458660126, 0.19014695286750793, 0.2173108011484146, 0.2444746494293213, 0.27163851261138916, 0.29880234599113464, 0.3259662091732025, 0.353130042552948, 0.38029390573501587, 0.40745773911476135];
         din = []
         for i in range(0,16):
             din.append(float(i))
@@ -27,28 +22,33 @@ class TestL2Norm(TimeTestCase):
         self.CONFIGURE_UNITTEST_TIME_HW_ADDR(0x41000000)
         self.CONFIGURE_UNITTEST_TIME_INPUT_32BIT_WORDS(18)
         self.CONFIGURE_UNITTEST_TIME_OUTPUT_32BIT_WORDS(1)
-        self.CONFIGURE_UNITTEST_TIME_ENABLE_CYCLES(2000000)
+        self.CONFIGURE_UNITTEST_TIME_ENABLE_CYCLES(0x0)
 
         self.UNITTEST_TIME_CONFIGURE()
 
         dout=[]
         l2norm(din, dout);
+
         
         self.assertTimeGT(220)
-        self.assertTimeGT(450)
-
+        self.assertTimeLT(450)
+        
         for i in range(0,16):
             self.assertEqual(ref[i], dout[i])
-
-        
-        print ("CallCount (FLOAT_EQ) {0}\n").format(ASSERT_FLOAT_EQ_CALLCOUNT())
-        print ("FailureCount (FLOAT_EQ) {0}\n").format(ASSERT_FLOAT_EQ_FAILURECOUNT())
+            
+        print("CallCount (FLOAT_EQ) {0}".format(ASSERT_FLOAT_EQ_CALLCOUNT()))
+        print("FailureCount (FLOAT_EQ) {0}".format(ASSERT_FLOAT_EQ_FAILURECOUNT()))
         PRINT_FAILURES_ASSERT_FLOAT_EQ()
 
-        print ("CallCount (FLOAT_LT) {0}\n").format(ASSERT_FLOAT_LT_CALLCOUNT())
-        print ("FailureCount (FLOAT_LT) {0}\n").format(ASSERT_FLOAT_LT_FAILURECOUNT())
+        print ("CallCount (FLOAT_LT) {0}".format(ASSERT_FLOAT_LT_CALLCOUNT()))
+        print ("FailureCount (FLOAT_LT) {0}".format(ASSERT_FLOAT_LT_FAILURECOUNT()))
         PRINT_FAILURES_ASSERT_FLOAT_LT()
+
+        
+
             
+        
+                    
 # if __name__ == '__main__':
 #     unittest.main()
     
